b34b3c359b5fcbf059736e5116d3380d95cdf2af
Rishabh <rishabhkaushal1796@gmail.com>
2022-05-06 21:15:34 +0530

    Deleting websites

--- website-blocker.py
+++ website-blocker.py
@@ -7,7 +7,7 @@ redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com"]
 
 while True: #to keep the script running all the time.
-    if dt(dt.now().year,dt.now().month,dt.now().day,20) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day+1,1):
+    if dt(dt.now().year,dt.now().month,dt.now().day,22) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day+1,1):
         print("Working hours")
         with open(hosts_temp,'r+') as file:
             content=file.read()
@@ -17,5 +17,12 @@ while True: #to keep the script running all the time.
                 else:
                     file.write(redirect+" "+website+"\n")
     else:
+        with open(hosts_temp,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
         print("Fun hours")
     time.sleep(5)
